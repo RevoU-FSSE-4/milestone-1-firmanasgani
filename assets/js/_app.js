796421bf72a4cf74547c3132aeb125ace3e11893
firman maulana <firmanasgani37@gmail.com>
2024-03-07 12:14:23 +0700

    add readme

--- assets/js/_app.js
+++ assets/js/_app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+const API_URL = "https://quotes.crowintheglass.com";
 
 function index() {
   let html = `  
@@ -27,7 +27,7 @@ function getQuotes() {
       data.map((row) => {
         html = html + `
         <article>
-          <img src="${API_URL}${row.picture}" class="img-article" alt="${row.charaName}">
+          <img src="${API_URL}/public/${row.picture}" class="img-article" alt="${row.charaName}">
           <p class="text-quote">"${row.quote}"</p>
           <p class="text-name">${row.charaName}</p>
         </article>`
@@ -59,12 +59,36 @@ function getPeople(param) {
     data.map((row) => {
       html = html + `
         <div class="people">
-          <img src="${API_URL}${row.pathPict}" alt="${row.altPict}" class="img-article">
-          <p>${row.charaName}</p>
+          <img src="${API_URL}/public/${row.pathPict}" alt="${row.altPict}" class="img-article">
+          <p class="text-quote">${row.charaName}</p>
         </div>
       `
     })
 
     document.getElementById('data-row').innerHTML = html
   })
+}
+
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+function addSubs()  {
+  var email = document.getElementById('email')
+  if(email.value === '') {
+    return alert('Email is blank')
+  }
+
+  if(!validateEmail(email.value)) {
+    return alert('That is not email')
+  }
+
+  document.getElementById('text').innerText = 'Subscribed!'
+  alert('Hi ' + email.value + ', please check your email for verification.')
+
+  setTimeout(function() {
+    document.getElementById('text').innerText = 'Subscribe'
+    email.value = ''
+  }, 5000)
 }
