cd1d1f41389ce629e3e26cd6c647bbf46300cde1
firman maulana <firmanasgani37@gmail.com>
2024-03-07 20:56:43 +0700

    update readme

--- assets/js/_app.js
+++ assets/js/_app.js
@@ -1,20 +1,5 @@
 const API_URL = "https://quotes.crowintheglass.com";
 
-function index() {
-  let html = `  
-    <article>
-        <img src="assets/img/albert_camus1.webp" class="img-article">
-        <p class="text-quote">"There is no need for you to end this war. There is no need for you to end this war</p>
-  </article>`;
-
-  let article = "";
-
-  for (i = 0; i < 6; i++) {
-    article = article + html;
-  }
-
-  document.getElementById("article").innerHTML = article;
-}
 
 function getQuotes() {
   let html = ''
